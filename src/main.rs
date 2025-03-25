a612f3d99b1bf5cedcfa7b90c7d8a6b3f328b988
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-03-24 22:56:00 -0300

    more of bit manipulation than anything else

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod bitwise_or;
 mod count_nice_subarrays;
+mod minimum_op_reduce_array_zero;
+mod monotonic_stack;
 mod subarray_sum_k;
 mod subarry_sum_min;
-mod monotonic_stack;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/minimum_op_reduce_array_zero.rs
@@ -0,0 +1,19 @@
+//! 2571. Minimum Operations to Reduce an Integer to 0
+
+struct Solution;
+
+impl Solution {
+    pub fn min_operations(mut n: i32) -> i32 {
+        let mut count = 0;
+        while n > 0 {
+            if n & 3 == 3 {
+                n += 1;
+                count += 1;
+            } else {
+                count += n & 1;
+                n >>= 1;
+            }
+        }
+        count
+    }
+}
